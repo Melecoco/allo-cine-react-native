b61422f8faf5dee71fa38d77546ffa324231a625
Unknown <duhalgouetm@yahoo.com>
2019-11-07 14:26:40 +0100

    correct import

--- pageComponents/HomePage.js
+++ pageComponents/HomePage.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import { StyleSheet, Text, View,  Button} from 'react-native';
 import CardCreator from '../components/CardCreator'
-
+import FilmList from './../components/FilmList'
 const HomePage = (props) => {
     const {navigation} = props
 
