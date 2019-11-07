39812e734bed8be68d2d767c7abc92c94daf25cf
Ben <bengrandin@hotmail.com>
2019-11-07 22:51:48 +0100

    trying To make and Navbar & part of About

--- App.js
+++ App.js
@@ -1,30 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { SearchBar } from 'react-native-elements';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 import HomePage from './pageComponents/HomePage'
 import MyFilmsPage from './pageComponents/MyFilmsPage'
 import DetailFilmPage from './pageComponents/DetailFilmPage'
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import About from "./pageComponents/About";
+import {Button} from "react-native";
 
 
 const AppNavigator = createStackNavigator(
-  {
-  Home: HomePage,
-  MyFilms: MyFilmsPage,
-  DetailFilm: DetailFilmPage
-  },
-  {
-    //nitialRouteName: 'Home'
-    initialRouteName: 'Home'
-  }
+	{
+		Home: HomePage,
+		MyFilms: MyFilmsPage,
+		DetailFilm: DetailFilmPage,
+		About: About
+	},
+	{
+		initialRouteName: 'Home',
+		defaultNavigationOptions: {
+			title: "Awesome Movies",
+
+			/* ToDo: Implement headerRight to active navigation
+			-> Cant use navigate whereas createStackNavigator isn't launch.
+			-> Don't want to implement it manually for each view
+			-> Cant use bottomNav and stackNav
+				headerRight: () => <HeaderRight/>,
+			 */
+			headerRight: () => (
+				<Button
+					onPress={() => alert('This is a button!')}
+					title="About"
+				/>),
+			headerStyle: {
+				backgroundColor: '#000000',
+			},
+			headerTintColor: '#fff',
+			headerTitleStyle: {
+				fontWeight: 'bold',
+			},
+		},
+	}
 );
+
+const TabNavigator = createBottomTabNavigator({
+	Home: HomePage,
+	Settings: MyFilmsPage,
+});
 
 const AppContainer = createAppContainer(AppNavigator);
 
 
 export default class App extends React.Component {
-  render() {
-    return <AppContainer />;
-  }
+	render() {
+		return <AppContainer/>;
+	}
 }
---
+++ components/NavBar.js
@@ -0,0 +1,13 @@
+export default TabNavigator
+
+// export default class NavBar extends Component {
+// 	render() {
+// 		return (
+// 			<Navigator
+// 				initialRoute={{title: 'Awesome Scene', index: 0}}
+// 				renderScene={(route, navigator) => <Text>Hello {route.title}!</Text>}
+// 				style={{padding: 100}}
+// 			/>
+// 		);
+// 	}
+// }
