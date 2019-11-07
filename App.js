f444812986d29e71da257cba7d806541d145a4b8
Unknown <boubacar.yatera@edu.itescia.fr>
2019-11-07 15:38:51 +0100

    Searchbar ok

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { SearchBar } from 'react-native-elements';
 import HomePage from './pageComponents/HomePage'
 import MyFilmsPage from './pageComponents/MyFilmsPage'
 import DetailFilmPage from './pageComponents/DetailFilmPage'
--- pageComponents/HomePage.js
+++ pageComponents/HomePage.js
@@ -1,13 +1,18 @@
 import React, {useState, useEffect} from 'react'
 import { StyleSheet, Text, View,  Button} from 'react-native';
 import CardCreator from '../components/CardCreator'
-import FilmList from './../components/FilmList'
+import FilmList from '../components/FilmList'
+import Searchbar from '../components/Searchbar'
+
+
 const HomePage = (props) => {
+
     const {navigation} = props
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen HELLO</Text>
+            <Searchbar></Searchbar>
+            <Text>HomePage</Text>
             <FilmList></FilmList>
         </View>
 
