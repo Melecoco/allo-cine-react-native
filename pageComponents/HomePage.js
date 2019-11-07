0b3c4ae9c5e78cca9a62e02490f89097f5a102a5
Ben <bengrandin@hotmail.com>
2019-11-07 13:55:45 +0100

    rename foldes

--- pageComponents/HomePage.js
+++ pageComponents/HomePage.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import { StyleSheet, Text, View,  Button} from 'react-native';
 import CardCreator from '../components/CardCreator'
+
 const HomePage = (props) => {
     const {navigation} = props
 
@@ -9,7 +10,7 @@ const HomePage = (props) => {
             <Text>Home Screen HELLO</Text>
             <CardCreator></CardCreator>
         </View>
-        
+
     )
 }
 export default HomePage
