ec5e8bd3339eb912843b3044f5c785c24be46b29
Unknown <boubacar.yatera@edu.itescia.fr>
2019-11-08 11:16:39 +0100

    DetailFilms et About

--- pageComponents/DetailFilmPage.js
+++ pageComponents/DetailFilmPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {Image, ScrollView, StyleSheet, Text} from 'react-native';
-import {Button, Icon} from 'react-native-elements'
+import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {Button, Icon, Card} from 'react-native-elements'
 import {getPosterFromApi} from "../API/TMDB";
 
 const DetailFilmPage = (props) => {
@@ -8,21 +8,30 @@ const DetailFilmPage = (props) => {
 	const film = props.navigation.getParam("film");
 	return (
 
-		<ScrollView>
-			<Image style={{width: "100%", height: "100%"}} resizeMode='contain'
-				   source={{uri: getPosterFromApi(film.poster)}}/>
 
-			<Text style={{fontSize:30, textAlign:"center", marginBottom: 10, fontWeight: 'bold'}}> {film.title} </Text>
-			<Text style={{marginBottom: 10, fontWeight: 'bold'}}> Synopsis : </Text>
-			<Text style={{marginBottom: 10}}> {film.synopsis} </Text>
-			<Text style={{marginBottom: 10, fontWeight: 'bold'}}> Date de sortie : {film.releaseYear} </Text>
 
-			<Button
-				icon={<Icon name='code' color='#ffffff'/>}
 
-				buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-				title='Like'/>
-		</ScrollView>
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+                    <Card>
+
+                    <Image style={{width: "100%", height: "50%"}} source={{uri:getPosterFromApi(film.poster)}}/>
+
+                    <Text style={{marginBottom: 10, fontWeight: 'bold', textAlign:"center", fontSize:30}} > {film.title} </Text>
+                    <Text style={{marginBottom: 10, fontWeight: 'bold', fontSize:15}} > Date de sortie :  {film.releaseYear} </Text>
+                    <Text style={{marginBottom: 10, fontWeight: 'bold', fontSize:15}}> Synopsis : </Text>
+                    <ScrollView>
+                    <Text style={{marginBottom: 10,textAlign:"center"}}> {film.synopsis} </Text>
+                    </ScrollView>
+                      <Button
+                        icon={<Icon name='code' color='#ffffff' />}
+
+                        buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                        title='Like' />
+
+                    </Card>
+
+                </View>
 	)
 };
 
