995f75585619e8b12f4a33a76260912639b9b9fe
Unknown <duhalgouetm@yahoo.com>
2019-11-08 11:16:49 +0100

    Header header

--- pageComponents/HomePage.js
+++ pageComponents/HomePage.js
@@ -3,6 +3,7 @@ import React, {useEffect, useState} from 'react'
 import {Text, View} from 'react-native';
 import FilmList from './../components/FilmList'
 import Searcher from '../components/Searcher'
+import HeaderRight from '../components/HeaderRight'
 
 let isHomeScreenHaventAlreadyLoaded = true;
 const HomePage = (props) => {
@@ -25,6 +26,7 @@ const HomePage = (props) => {
 
 	return (
 		<View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}} >
+            <HeaderRight navigation={navigation}></HeaderRight>
 			<Text h1 style={{fontSize:30, textAlign:"center", marginBottom: 10, fontWeight: 'bold'}}>Welcome ! </Text>
 			<Searcher onComplete={setFilms}/>
 			<FilmList navigation={navigation} films={films}/>
